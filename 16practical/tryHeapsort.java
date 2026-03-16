6aaa09847f65e7524d3f62bf6378b0bff4cb96c1
faith101starr <4561731@myuwc.ac.za>
2026-03-16 15:36:59 +0200

    Added Top-Down Heap

--- 16practical/tryHeapsort.java
+++ 16practical/tryHeapsort.java
@@ -10,7 +10,7 @@ public class tryHeapsort{
 
     //The number of elements currently in the heap
 
-    public static void main (String [] args) throws Exception{
+    public static void main (String [] args) throws Exception {
 
         //Reading sorted list from the cleaned file anagrams_output.txt
 
@@ -18,16 +18,63 @@ public class tryHeapsort{
 
         // BOTTOM-UP HEAP
 
-        String [] bottomHeap = words.clone();
+        String[] bottomHeap = words.clone();
 
-        long stratBottom = System.nanoTime();
+        long startBottom = System.nanoTime();
 
         buildHeapBottomUp(BottomHeap);
         heapSort(bottomHeap);
 
-        long startBottom = System.nanoTime();
+        long endBottom = System.nanoTime();
 
-        
+        System.out.println("Bottom-Up Sorted keys :");
+
+        printArray(bottomHeap);
+        System.out.println("Bottom-Up Time: " + (endBottom - startBottom) + " ns");
+
+        //TOP-DOWN HEAP
+
+        String[] topHeap = words.clone();
+
+        long startTop = System.nanoTime();
+
+        buildHeapTopDown(topHeap);
+        heapSort(topHeap);
+
+        long endTop = System.nanoTime();
+
+        System.out.println("\nSorted keys (Top-Down): ");
+        printArray(topHeap);
+        System.out.println("Top-Down Time: " + (endTop - startTop) + " ns");
+
+    }
+
+    static String[] readKeys(String filename) throws Exception{
+        List<String>keys = new ArrayList<>();
+
+        BufferedReader reader = new BufferedReader(new FileReader (filename))
+                String line;
+
+        while((line = reader.readLine()) != null){
+            int colon = line.indexOf(":");
+
+            if (colon != -1) {
+                String key = line.substring(0, colon);
+                keys.add(key);
+            }
+        }
+
+        reader.close();
+
+        return keys.toArray(new String[0]);
+    }
+
+    //----------------------------------------
+    //BOTTOM-UP HEAP CONSTRUCTION
+
+
+        }
+
 
     }
 
